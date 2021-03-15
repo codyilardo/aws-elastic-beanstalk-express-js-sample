74adaf58ad3150ff47b6ff557d7c4e6b0be80e5a
Cody Ilardo <cilardo@stetson.edu>
2021-03-15 13:14:10 -0400

    updates to app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Its a cheetah! Oh wait, its Henry!'));
+app.get('/', (req, res) => res.send('Its a cheetah! Its a jaguar! Oh wait, its Henry!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
